ea403a4d6ef4ea74f46dee6144ca105cba94a26b
tanipumas <abraham.unotv@gmail.com>
2026-07-03 14:37:33 -0600

    cambio

--- backend/backend_maiz/settings.py
+++ backend/backend_maiz/settings.py
@@ -45,10 +45,12 @@ ROOT_URLCONF = 'backend_maiz.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        # Ajustamos para que busque en la raíz, donde está tu carpeta templates
+        'DIRS': [BASE_DIR / 'templates'], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
