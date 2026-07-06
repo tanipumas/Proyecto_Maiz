fe3656b51348072fff0c68da9b2d8d6331752824
tanipumas <abraham.unotv@gmail.com>
2026-07-06 09:35:34 -0600

    1

--- backend/backend_maiz/wsgi.py
+++ backend/backend_maiz/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend_maiz.settings')
+# Agregamos el directorio raíz y la carpeta backend al path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_maiz.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 def main():
+    sys.path.append(os.path.join(os.path.dirname(__file__), 'backend'))
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend_maiz.settings')
     try:
         from django.core.management import execute_from_command_line
