52c962e5119cb97d8cd0a6c6ba89b4b6012cefe5
tanipumas <abraham.unotv@gmail.com>
2026-07-07 07:51:26 -0600

    20

--- backend/backend_maiz/settings.py
+++ backend/backend_maiz/settings.py
@@ -77,7 +77,11 @@ USE_TZ = True
 # Static files
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'), 
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
